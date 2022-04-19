1c8f3cf8927ce5a901927cc03f43a2d5a59f191e
Svyatoslav Beletskiy <svyatoslav.beletskiy@metropolia.fi>
2022-04-19 12:20:47 +0300

    step 1

---
+++ main/test.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+let server = app.listen(8081, function () {
+    let host = server.address().address
+    let port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
